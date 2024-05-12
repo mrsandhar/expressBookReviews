4f0ef8eb6c59a670a3a579396419de3436658244
SimerJitSingh <86659427+mrsandhar@users.noreply.github.com>
2024-05-12 16:17:18 +0200

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,9 +18,9 @@ public_users.get('/',function (req, res) {
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    let filtered_isbn = Object.values(books).filter((book) => book.isbn === isbn);
+    let filtered_isbn = Object.values(books).filter((book) => book.isbn === isbn); //no ISBN in booksdb
     if(!Object.keys(filtered_isbn).length === 0) {
-        return res.send(books[filtered_isbn]);
+        return res.send(JSON.stringify(filtered_isbn));
     } 
     else {
         return res.send("No book found with this ISBN");
@@ -53,8 +53,14 @@ public_users.get('/title/:title',function (req, res) {
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let filtered_isbn = Object.values(books).filter((book) => book.isbn === isbn); // NO ISBN in booksdb
+    if(!Object.keys(filtered_isbn).length === 0) {
+        return res.send(filtered_isbn.reviews);
+    } 
+    else {
+        return res.send("No reviews found for this ISBN");
+    }
 });
 
 module.exports.general = public_users;
