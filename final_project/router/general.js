11c3d247e8c5e80916cbd7a8361d49ed96ef0830
SimerJitSingh <86659427+mrsandhar@users.noreply.github.com>
2024-05-12 15:43:05 +0200

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -12,25 +12,43 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  return res.send(JSON.stringify({books}, null, 5));
+    return res.send(JSON.stringify({books}, null, 5));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    const isbn = req.params.isbn;
+    let filtered_isbn = Object.values(books).filter((book) => book.isbn === isbn);
+    if(!Object.keys(filtered_isbn).length === 0) {
+        return res.send(books[filtered_isbn]);
+    } 
+    else {
+        return res.send("No book found with this ISBN");
+    }
+});
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+    let keys = Object.keys(books);
+    for(let i=1; i<=keys.length; i++) {
+        if (author === books[i].author) {
+            return res.send(books[i]);
+        }
+    }
+    return res.send("No books by this author found");
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let keys = Object.keys(books);
+    for(let i=1; i<=keys.length; i++) {
+        if (title === books[i].title) {
+            return res.send(books[i]);
+        }
+    }
+    return res.send("No books by this title found");
 });
 
 //  Get book review
